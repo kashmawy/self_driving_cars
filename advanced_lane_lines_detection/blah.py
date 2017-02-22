ba226c74b9fe2151e19edd75dc4afbc7aff805ec
Khalid Ashmawy <kash@uber.com>
2017-02-21 23:23:40 -0800

    cleanup

--- advanced_lane_lines_detection/blah.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import cv2
-from scipy.misc import imread
-
-input_image = imread('./test_images/test1.jpg')
-cv2.rectangle(input_image, (10, 10), (100, 100), (0, 0, 0), 2)
-plt.imshow(input_image)
-plt.show()
