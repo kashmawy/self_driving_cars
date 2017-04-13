4375bb1415a3dab80a673c6be03796ff4284067d
Khalid Ashmawy <kash@uber.com>
2017-04-13 15:23:39 -0700

    added extended kalman filter

---
+++ localization_extended_kalman_filter/src/FusionEKF.cpp
@@ -0,0 +1,154 @@
+#include "FusionEKF.h"
+#include "tools.h"
+#include "Eigen/Dense"
+#include <iostream>
+#define ALPHA 0.00001
+
+using namespace std;
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+using std::vector;
+
+/*
+ * Constructor.
+ */
+FusionEKF::FusionEKF() {
+  is_initialized_ = false;
+
+  previous_timestamp_ = 0;
+
+  noise_ax_ = 9;
+  noise_ay_ = 9;
+
+  R_laser_ = MatrixXd(2, 2);
+  R_laser_ << 0.0225, 0,
+              0, 0.0225;
+
+  R_radar_ = MatrixXd(3, 3);
+  R_radar_ << 0.09, 0, 0,
+              0, 0.0009, 0,
+              0, 0, 0.09;
+
+  H_laser_ = MatrixXd(2, 4);
+  H_laser_ << 1, 0, 0, 0,
+		      0, 1, 0, 0;
+
+  VectorXd x_ = VectorXd(4);
+
+  MatrixXd P_ = MatrixXd(4, 4);
+  P_ << 1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 1000, 0,
+        0, 0, 0, 1000;
+
+  MatrixXd F_ = MatrixXd(4, 4);
+  F_ << 1, 0, 1, 0,
+		0, 1, 0, 1,
+		0, 0, 1, 0,
+		0, 0, 0, 1;
+
+  MatrixXd Q_ = MatrixXd(4, 4);
+
+
+  ekf_.Init(
+    x_,
+    P_,
+    F_,
+    H_laser_,
+    Hj_,
+    R_laser_,
+    R_radar_,
+    Q_
+  );
+}
+
+/**
+* Destructor.
+*/
+FusionEKF::~FusionEKF() {}
+
+void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
+
+
+  /*****************************************************************************
+   *  Initialization
+   ****************************************************************************/
+  if (!is_initialized_) {
+    // first measurement
+    ekf_.x_ = VectorXd(4);
+
+    if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
+      /**
+      Convert radar from polar to cartesian coordinates and initialize state.
+      */
+      cout << "Initial EKF Measurement of type Radar." << endl;
+      float rho = measurement_pack.raw_measurements_[0];  // range
+      float phi = measurement_pack.raw_measurements_[1];  // bearing
+      float rho_dot = measurement_pack.raw_measurements_[2];  // rho velocity
+
+      float x = rho * cos(phi);
+      float y = rho * sin(phi);
+      float vx = rho_dot * cos(phi);
+      float vy = rho_dot * sin(phi);
+
+      ekf_.x_ << x, y, vx, vy;
+    }
+    else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
+      /**
+      Initialize state.
+      */
+      cout << "Initial EKF Measurement of type Laser." << endl;
+      ekf_.x_ << measurement_pack.raw_measurements_[0], measurement_pack.raw_measurements_[1], 0, 0;
+    }
+
+    if (fabs(ekf_.x_(0)) < ALPHA and fabs(ekf_.x_(1)) < ALPHA) {
+      ekf_.x_(0) = ALPHA;
+      ekf_.x_(1) = ALPHA;
+    }
+
+    // done initializing, no need to predict or update
+    is_initialized_ = true;
+    return;
+  }
+
+  /*****************************************************************************
+   *  Prediction
+   ****************************************************************************/
+
+  float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;
+
+  previous_timestamp_ = measurement_pack.timestamp_;
+
+  float dt_2 = dt * dt;
+  float dt_3 = dt_2 * dt;
+  float dt_4 = dt_3 * dt;
+
+  ekf_.F_(0, 2) = dt;
+  ekf_.F_(1, 3) = dt;
+
+  ekf_.Q_ = MatrixXd(4, 4);
+  ekf_.Q_ << dt_4/4*noise_ax_, 0, dt_3/2*noise_ax_, 0,
+             0, dt_4/4*noise_ay_, 0, dt_3/2*noise_ay_,
+             dt_3/2*noise_ax_, 0, dt_2*noise_ax_, 0,
+             0, dt_3/2*noise_ay_, 0, dt_2*noise_ay_;
+
+  ekf_.Predict();
+
+  /*****************************************************************************
+   *  Update
+   ****************************************************************************/
+
+  if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
+    // Radar updates
+    cout << "Update phase of EKF Measurement of type radar." << endl;
+    ekf_.UpdateEKF(measurement_pack.raw_measurements_);
+  } else {
+    // Laser updates
+    cout << "Update phase of EKF Measurement of type laser." << endl;
+    ekf_.Update(measurement_pack.raw_measurements_);
+  }
+
+  // print the output
+  cout << "x_ = " << ekf_.x_ << endl;
+  cout << "P_ = " << ekf_.P_ << endl;
+}
---
+++ localization_extended_kalman_filter/src/tools.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include "tools.h"
+
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
+using std::vector;
+
+using namespace std;
+
+Tools::Tools() {}
+
+Tools::~Tools() {}
+
+VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
+                              const vector<VectorXd> &ground_truth) {
+  cout << "RMSE" << endl;
+
+  VectorXd rmse(4);
+  rmse << 0, 0, 0, 0;
+
+  for (unsigned int i = 0; i < estimations.size(); ++i) {
+    VectorXd residual = estimations[i] - ground_truth[i];
+    residual = residual.array() * residual.array();
+    rmse += residual;
+  }
+
+  rmse = rmse / estimations.size();
+  rmse = rmse.array().sqrt();
+
+  return rmse;
+}
+
+MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
+  cout << "Jacobian" << endl;
+
+  MatrixXd Hj(3, 4);
+
+  float px = x_state(0);
+  float py = x_state(1);
+  float vx = x_state(2);
+  float vy = x_state(3);
+
+  float c1 = px*px + py*py;
+  float c2 = sqrt(c1);
+  float c3 = (c1 * c2);
+
+  if (fabs(c1) < 0.0001) {
+    cout << "CalculateJacobian () - Error - Division by Zero " << endl;
+    return Hj;
+  }
+
+  Hj << (px/c2), (py/2), 0, 0,
+        -(py/c1), (px/c1), 0, 0,
+        py*(vx*py - vy*px)/c3, px*(px*vy - py*vx)/c3, px/c2, py/c2;
+
+  return Hj;
+}
