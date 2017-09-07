0a10f67d634cac993fcdcce757c8dca5d1fd96e8
Khalid Ashmawy <khalid.ashmawy@gmail.com>
2017-09-07 07:25:55 -0700

    code review

--- semantic_segmantation/main.py
+++ semantic_segmantation/main.py
@@ -111,7 +111,7 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     labels = tf.reshape(correct_label, (-1, num_classes))
     cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels, name="Softmax"))
     train_op = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy_loss)
-    return logits, labels, cross_entropy_loss
+    return logits, train_op, cross_entropy_loss
 tests.test_optimize(optimize)
 
 
